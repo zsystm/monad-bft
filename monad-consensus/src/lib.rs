fa44f3d44ed828fec2543a8bbfa2e46ec56bca62
Ariq Chowdhury <ariqchowdhury@gmail.com>
2023-03-16 11:16:19 -0700

    add helper functions for safety checks

--- monad-consensus/src/lib.rs
+++ monad-consensus/src/lib.rs
@@ -1,16 +1,36 @@
 pub mod types;
+pub mod validation;
+
+use std::ops::Add;
+use std::ops::Sub;
 
 use zerocopy::AsBytes;
 
 type Hash = [u8; 32];
 
 #[repr(transparent)]
-#[derive(Copy, Clone, Debug, Default, PartialEq, PartialOrd, AsBytes)]
+#[derive(Copy, Clone, Debug, Default, Eq, Ord, PartialEq, PartialOrd, AsBytes)]
 pub struct Round(u64);
 
 impl AsRef<[u8]> for Round {
     fn as_ref(&self) -> &[u8] {
         self.0.as_bytes()
+    }
+}
+
+impl Add for Round {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self::Output {
+        Round(self.0 + other.0)
+    }
+}
+
+impl Sub for Round {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Round(self.0 - rhs.0)
     }
 }
 
---
+++ monad-consensus/src/validation/safety.rs
@@ -0,0 +1,68 @@
+use std::cmp;
+
+use crate::types::quorum_certificate::QcInfo;
+use crate::types::timeout::TimeoutCertificate;
+use crate::*;
+
+pub struct Safety {
+    highest_vote_round: Round,
+    highest_qc_round: Round,
+}
+
+impl Safety {
+    fn update_highest_vote_round(&mut self, r: Round) {
+        self.highest_vote_round = cmp::max(r, self.highest_vote_round);
+    }
+
+    fn update_highest_qc_round(&mut self, r: Round) {
+        self.highest_qc_round = cmp::max(r, self.highest_qc_round);
+    }
+
+    fn safe_to_vote(
+        self,
+        block_round: Round,
+        qc_round: Round,
+        tc: &Option<TimeoutCertificate>,
+    ) -> bool {
+        if block_round <= cmp::max(self.highest_vote_round, qc_round) {
+            return false;
+        }
+
+        return consecutive(block_round, qc_round) || safe_to_extend(block_round, qc_round, tc);
+    }
+
+    fn safe_to_timeout(
+        self,
+        round: Round,
+        qc_round: Round,
+        tc: &Option<TimeoutCertificate>,
+    ) -> bool {
+        if qc_round < self.highest_qc_round
+            || round <= cmp::max(self.highest_qc_round - Round(1), qc_round)
+        {
+            return false;
+        }
+
+        let consecutive_tc = match tc {
+            Some(t) => consecutive(round, t.round),
+            None => false,
+        };
+
+        return consecutive(round, qc_round) || consecutive_tc;
+    }
+}
+
+fn consecutive(block_round: Round, round: Round) -> bool {
+    round + Round(1) == block_round
+}
+
+fn safe_to_extend(block_round: Round, qc_round: Round, tc: &Option<TimeoutCertificate>) -> bool {
+    match tc {
+        Some(t) => consecutive(block_round, t.round) && qc_round >= t.max_round(),
+        None => false,
+    }
+}
+
+fn commit_condition(block_round: Round, qc: QcInfo) -> bool {
+    consecutive(block_round, qc.vote_info.round)
+}
