3d532b886e6e3d31f91ffdf7a420e34d668b12ed
Ariq Chowdhury <ariqchowdhury@gmail.com>
2023-03-16 12:30:11 -0700

    add validation funcs for wellformed messages

---
+++ monad-consensus/src/validation/message.rs
@@ -0,0 +1,35 @@
+use crate::*;
+
+use crate::types::message::{ProposalMessage, TimeoutMessage};
+use crate::types::timeout::TimeoutCertificate;
+use crate::types::voting::VotingQuorum;
+
+pub fn well_formed_proposal<T: VotingQuorum>(p: &ProposalMessage<T>) -> bool {
+    well_formed(
+        p.block.round,
+        p.block.qc.info.vote_info.round,
+        &p.last_round_tc,
+    )
+}
+
+pub fn well_formed_timeout<T: VotingQuorum>(t: &TimeoutMessage<T>) -> bool {
+    well_formed(
+        t.tminfo.round,
+        t.tminfo.high_qc.info.vote_info.round,
+        &t.last_round_tc,
+    )
+}
+
+// (DiemBFT v4, p.12)
+// https://developers.diem.com/papers/diem-consensus-state-machine-replication-in-the-diem-blockchain/2021-08-17.pdf
+fn well_formed(round: Round, qc_round: Round, tc: &Option<TimeoutCertificate>) -> bool {
+    let prev_round = round - Round(1);
+    if qc_round != prev_round {
+        match tc {
+            Some(t) => t.round == prev_round,
+            None => false,
+        }
+    } else {
+        tc.is_none()
+    }
+}
