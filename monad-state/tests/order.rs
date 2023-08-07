25707847338813167b0cd57290199a5f54a88db1
Justin Cai <jc@Justins-MacBook-Pro.local>
2023-08-07 14:57:50 -0400

    update according to comment

--- monad-state/tests/order.rs
+++ monad-state/tests/order.rs
@@ -1,69 +1,62 @@
-#[cfg(test)]
-mod test {
-    use std::{collections::HashSet, env, time::Duration};
+use std::{collections::HashSet, env, time::Duration};
 
-    use monad_executor::{
-        mock_swarm::{LatencyTransformer, Transformer},
-        PeerId,
+use monad_executor::{
+    mock_swarm::{LatencyTransformer, Transformer},
+    PeerId,
+};
+use monad_testutil::swarm::{
+    get_configs, run_one_delayed_node, PartitionThenReplayTransformer, TransformerReplayOrder,
+};
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use test_case::test_case;
+#[test]
+#[ignore = "cron_test"]
+fn all_messages_delayed_cron() {
+    let round = match env::var("ALL_MESSAGES_DELAYED_ROUND") {
+        Ok(v) => v.parse().unwrap(),
+        Err(_e) => panic!("ALL_MESSAGES_DELAYED_ROUND is not set"),
     };
-    use monad_testutil::swarm::{
-        get_configs, run_one_delayed_node, PartitionThenReplayTransformer, TransformerReplayOrder,
+
+    match env::var("RANDOM_TEST_SEED") {
+        Ok(v) => {
+            let mut seed = v.parse().unwrap();
+            let mut generator = StdRng::seed_from_u64(seed);
+            for _ in 0..round {
+                seed = generator.gen();
+                println!("seed for all_messages_delayed_cron is set to be {}", seed);
+                all_messages_delayed(TransformerReplayOrder::Random(seed));
+            }
+        }
+        Err(_e) => panic!("RANDOM_TEST_SEED is not set"),
     };
-    use rand::{rngs::StdRng, Rng, SeedableRng};
-    use test_case::test_case;
+}
+#[test_case(TransformerReplayOrder::Forward; "in order")]
+#[test_case(TransformerReplayOrder::Reverse; "reverse order")]
+#[test_case(TransformerReplayOrder::Random(1); "random seed 1")]
+#[test_case(TransformerReplayOrder::Random(2); "random seed 2")]
+#[test_case(TransformerReplayOrder::Random(3); "random seed 3")]
+#[test_case(TransformerReplayOrder::Random(4); "random seed 4")]
+#[test_case(TransformerReplayOrder::Random(5); "random seed 5")]
+fn all_messages_delayed(direction: TransformerReplayOrder) {
+    let num_nodes = 4;
+    let delta = Duration::from_millis(2);
+    let (pubkeys, state_configs) = get_configs(num_nodes, delta);
 
-    #[test_case(TransformerReplayOrder::Forward; "in order")]
-    #[test_case(TransformerReplayOrder::Reverse; "reverse order")]
-    #[test_case(TransformerReplayOrder::Random(1); "random seed 1")]
-    #[test_case(TransformerReplayOrder::Random(2); "random seed 2")]
-    #[test_case(TransformerReplayOrder::Random(3); "random seed 3")]
-    #[test_case(TransformerReplayOrder::Random(4); "random seed 4")]
-    #[test_case(TransformerReplayOrder::Random(5); "random seed 5")]
-    fn all_messages_delayed(direction: TransformerReplayOrder) {
-        let num_nodes = 4;
-        let delta = Duration::from_millis(2);
-        let (pubkeys, state_configs) = get_configs(num_nodes, delta);
+    assert!(num_nodes >= 2, "test requires 2 or more nodes");
 
-        assert!(num_nodes >= 2, "test requires 2 or more nodes");
+    let first_node = PeerId(*pubkeys.first().unwrap());
 
-        let first_node = PeerId(*pubkeys.first().unwrap());
+    let mut filter_peers = HashSet::new();
+    filter_peers.insert(first_node);
 
-        let mut filter_peers = HashSet::new();
-        filter_peers.insert(first_node);
+    println!("delayed node ID: {:?}", first_node);
 
-        println!("delayed node ID: {:?}", first_node);
-
-        run_one_delayed_node(
-            vec![
-                LatencyTransformer(Duration::from_millis(1)).boxed(),
-                PartitionThenReplayTransformer::new(filter_peers, 200, direction).boxed(),
-            ],
-            pubkeys,
-            state_configs,
-        );
-    }
-
-    #[test]
-    fn all_messages_delayed_cron() {
-        let round = match env::var("ALL_MESSAGES_DELAYED_ROUND") {
-            Ok(v) => v.parse().unwrap(),
-            Err(_e) => 1, // default to 1 if not found
-        };
-
-        match env::var("RANDOM_TEST_SEED") {
-            Ok(v) => {
-                let mut seed = v.parse().unwrap();
-                let mut generator: StdRng = StdRng::seed_from_u64(seed);
-                for _ in 0..round {
-                    seed = generator.gen();
-                    println!("seed for all_messages_delayed_cron is set to be {}", seed);
-                    all_messages_delayed(TransformerReplayOrder::Random(seed));
-                }
-            }
-            Err(_e) => {
-                println!("RANDOM_TEST_SEED is not set, default it to inorder");
-                all_messages_delayed(TransformerReplayOrder::Forward);
-            }
-        };
-    }
+    run_one_delayed_node(
+        vec![
+            LatencyTransformer(Duration::from_millis(1)).boxed(),
+            PartitionThenReplayTransformer::new(filter_peers, 200, direction).boxed(),
+        ],
+        pubkeys,
+        state_configs,
+    );
 }
--- monad-state/tests/rand_lat.rs
+++ monad-state/tests/rand_lat.rs
@@ -1,55 +1,52 @@
-#[cfg(test)]
-mod test {
-    use std::env;
+use std::env;
 
-    use monad_testutil::swarm::run_nodes;
-    use rand::{rngs::StdRng, Rng, SeedableRng};
-    use test_case::test_case;
+use monad_testutil::swarm::run_nodes;
+use rand::{rngs::StdRng, Rng, SeedableRng};
+use test_case::test_case;
 
-    #[test_case(1; "seed1")]
-    #[test_case(2; "seed2")]
-    #[test_case(3; "seed3")]
-    #[test_case(4; "seed4")]
-    #[test_case(5; "seed5")]
-    #[test_case(6; "seed6")]
-    #[test_case(7; "seed7")]
-    #[test_case(8; "seed8")]
-    #[test_case(9; "seed9")]
-    #[test_case(10; "seed10")]
-    fn nodes_with_random_latency(seed: u64) {
-        use std::time::Duration;
+#[test]
+#[ignore = "cron_test"]
+fn nodes_with_random_latency_cron() {
+    let round = match env::var("NODES_WITH_RANDOM_LATENCY_ROUND") {
+        Ok(v) => v.parse().unwrap(),
+        Err(_e) => panic!("NODES_WITH_RANDOM_LATENCY_ROUND is not set"), // default to 1 if not found
+    };
 
-        use monad_executor::mock_swarm::RandLatencyTransformer;
-
-        run_nodes(
-            4,
-            2048,
-            Duration::from_millis(250),
-            RandLatencyTransformer::new(seed, 330),
-        );
-    }
-
-    #[test]
-    fn nodes_with_random_latency_cron() {
-        let round = match env::var("NODES_WITH_RANDOM_LATENCY_ROUND") {
-            Ok(v) => v.parse().unwrap(),
-            Err(_e) => 1, // default to 1 if not found
-        };
-
-        match env::var("RANDOM_TEST_SEED") {
-            Ok(v) => {
-                let mut seed = v.parse().unwrap();
-                let mut generator: StdRng = StdRng::seed_from_u64(seed);
-                for _ in 0..round {
-                    seed = generator.gen();
-                    println!("seed is set to be {}", seed);
-                    nodes_with_random_latency(seed);
-                }
+    match env::var("RANDOM_TEST_SEED") {
+        Ok(v) => {
+            let mut seed = v.parse().unwrap();
+            let mut generator = StdRng::seed_from_u64(seed);
+            for _ in 0..round {
+                seed = generator.gen();
+                println!("seed is set to be {}", seed);
+                nodes_with_random_latency(seed);
             }
-            Err(_e) => {
-                println!("RANDOM_TEST_SEED is not set, default it to 0");
-                nodes_with_random_latency(0);
-            }
-        };
-    }
+        }
+        Err(_e) => {
+            panic!("RANDOM_TEST_SEED is not set");
+        }
+    };
+}
+
+#[test_case(1; "seed1")]
+#[test_case(2; "seed2")]
+#[test_case(3; "seed3")]
+#[test_case(4; "seed4")]
+#[test_case(5; "seed5")]
+#[test_case(6; "seed6")]
+#[test_case(7; "seed7")]
+#[test_case(8; "seed8")]
+#[test_case(9; "seed9")]
+#[test_case(10; "seed10")]
+fn nodes_with_random_latency(seed: u64) {
+    use std::time::Duration;
+
+    use monad_executor::mock_swarm::RandLatencyTransformer;
+
+    run_nodes(
+        4,
+        2048,
+        Duration::from_millis(250),
+        RandLatencyTransformer::new(seed, 330),
+    );
 }
