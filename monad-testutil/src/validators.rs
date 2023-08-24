e6c8e9a63b26551bcb7afeb2716bf1cb0e7f5bc5
Aashish Welling <aashishwelling@gmail.com>
2023-08-24 16:19:44 -0400

    Remove unused MockLeaderElection

--- monad-testutil/src/validators.rs
+++ monad-testutil/src/validators.rs
@@ -4,32 +4,10 @@ use monad_consensus_types::{
     voting::ValidatorMapping,
 };
 use monad_crypto::secp256k1::KeyPair;
-use monad_types::{NodeId, Round, Stake};
-use monad_validator::{
-    leader_election::LeaderElection,
-    validator_set::{ValidatorSet, ValidatorSetType},
-};
+use monad_types::{NodeId, Stake};
+use monad_validator::validator_set::{ValidatorSet, ValidatorSetType};
 
 use crate::signing::{create_certificate_keys, create_keys};
-
-pub struct MockLeaderElection {
-    leader: NodeId,
-}
-
-impl LeaderElection for MockLeaderElection {
-    fn new() -> Self {
-        let mut key: [u8; 32] = [128; 32];
-        let keypair = KeyPair::from_bytes(&mut key).unwrap();
-        let leader = keypair.pubkey();
-        MockLeaderElection {
-            leader: NodeId(leader),
-        }
-    }
-
-    fn get_leader(&self, _round: Round, _validator_list: &[NodeId]) -> NodeId {
-        self.leader
-    }
-}
 
 pub fn create_keys_w_validators<SCT: SignatureCollection>(
     num_nodes: u32,
