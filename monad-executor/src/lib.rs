6184793a7016ad5215db8937d4a884729a0c6162
Aashish Welling <aashishwelling@gmail.com>
2023-09-21 15:17:00 -0400

    Remove obsolete driver loop function

--- monad-executor/src/lib.rs
+++ monad-executor/src/lib.rs
@@ -1,5 +1,3 @@
-use futures::{Stream, StreamExt};
-
 mod state;
 pub use state::*;
 
@@ -10,31 +8,3 @@ pub mod timed_event;
 pub mod transformer;
 
 pub mod convert;
-
-// driver loop
-async fn run<S: State>(
-    mut executor: impl Executor<Command = Command<S::Message, S::OutboundMessage, S::Block, S::Checkpoint>>
-        + Stream<Item = S::Event>
-        + Unpin,
-    config: S::Config,
-    init_events: Vec<S::Event>,
-) {
-    let (mut state, mut init_commands) = S::init(config);
-    for event in init_events {
-        let cmds = state.update(event);
-        init_commands.extend(cmds);
-    }
-    executor.exec(init_commands);
-
-    while let Some(event) = executor.next().await {
-        let commands = state.update(event);
-
-        // TODO persist event (append)
-        // observation: only need to flush if commands is nonempty AND commands contains
-        //              something important (must be committed - eg RouterCommand::Publish)
-        //
-        // other: does sending an ack require state change?
-
-        executor.exec(commands);
-    }
-}
