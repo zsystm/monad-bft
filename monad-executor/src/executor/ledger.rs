f4a4d5b4b3aa58ecdf73bd4df164e4b11a43b0bc
Justin Cai <cai2457085598@gmail.com>
2023-09-27 14:14:43 -0400

    Add Fuzz Testing for Mock ledger
    Previously, MockLedger lack consideration for the fact that requests
    for block might not be instant, and order of arrival of executor
    command is not sequential, this problem is addressed in pr #279.
    
    This commit contains additional unit test to ensure such behaviour is
    tested, Its worth mentioning that if identical ledgerFetch is observed
    before the previous one is polled, then the new ledgerFetch is will
    not be answered. (when given identical request at the same time, only
    the first request is answered)

--- monad-executor/src/executor/ledger.rs
+++ monad-executor/src/executor/ledger.rs
@@ -106,14 +106,51 @@ where
 }
 #[cfg(test)]
 mod tests {
+    use std::{
+        collections::{hash_map::Entry, HashMap, HashSet},
+        env,
+    };
+
     use futures::{FutureExt, StreamExt};
     use monad_testutil::{block::MockBlock, signing::get_key};
+    use monad_types::{BlockId, NodeId};
+    use rand::{seq::SliceRandom, Rng, SeedableRng};
+    use rand_chacha::ChaCha20Rng;
+    use test_case::test_case;
 
     use crate::{executor::ledger::MockLedger, Executor, LedgerCommand};
 
     #[derive(Debug, PartialEq, Eq)]
     struct MockLedgerEvent {
+        pub requester: NodeId,
+        pub bid: BlockId,
         pub block: Option<MockBlock>,
+    }
+
+    #[test]
+    #[ignore = "cron_test"]
+    fn test_ledger_command_arbitrary_order_cron() {
+        let round = match env::var("LEDGER_COMMAND_ARBITRARY_ORDER_ROUND") {
+            Ok(v) => v.parse().unwrap(),
+            Err(_e) => panic!("LEDGER_COMMAND_ARBITRARY_ORDER_ROUND is not set"),
+        };
+
+        match env::var("RANDOM_TEST_SEED") {
+            Ok(v) => {
+                let mut seed = v.parse().unwrap();
+                let mut generator = ChaCha20Rng::seed_from_u64(seed);
+                for _ in 0..round {
+                    seed = generator.gen();
+                    let nodes_req_range = generator.gen_range(0..1000);
+                    println!(
+                        "test_ledger_command_arbitrary_order, seed: {}, range: {} ",
+                        seed, nodes_req_range
+                    );
+                    test_ledger_command_arbitrary_order(seed, nodes_req_range);
+                }
+            }
+            Err(_e) => panic!("RANDOM_TEST_SEED is not set"),
+        };
     }
 
     #[test]
@@ -130,7 +167,11 @@ mod tests {
         mock_ledger.exec(vec![LedgerCommand::LedgerFetch(
             monad_types::NodeId(get_key(0).pubkey()),
             monad_types::BlockId(monad_types::Hash([0x00_u8; 32])),
-            Box::new(|block: Option<MockBlock>| MockLedgerEvent { block }),
+            Box::new(|block: Option<MockBlock>| MockLedgerEvent {
+                requester: monad_types::NodeId(get_key(0).pubkey()),
+                bid: monad_types::BlockId(monad_types::Hash([0x00_u8; 32])),
+                block,
+            }),
         )]);
         let retrieved = mock_ledger.next().now_or_never();
         assert_ne!(retrieved, None); // there should be a response
@@ -172,7 +213,11 @@ mod tests {
         mock_ledger.exec(vec![LedgerCommand::LedgerFetch(
             monad_types::NodeId(get_key(0).pubkey()),
             monad_types::BlockId(monad_types::Hash([0x02_u8; 32])),
-            Box::new(|block: Option<MockBlock>| MockLedgerEvent { block }),
+            Box::new(|block: Option<MockBlock>| MockLedgerEvent {
+                requester: monad_types::NodeId(get_key(0).pubkey()),
+                bid: monad_types::BlockId(monad_types::Hash([0x00_u8; 32])),
+                block,
+            }),
         )]);
         let retrieved = mock_ledger.next().now_or_never();
         assert_ne!(retrieved, None); // there should be a response
@@ -190,7 +235,11 @@ mod tests {
         mock_ledger.exec(vec![LedgerCommand::LedgerFetch(
             monad_types::NodeId(get_key(0).pubkey()),
             monad_types::BlockId(monad_types::Hash([0x02_u8; 32])),
-            Box::new(|block: Option<MockBlock>| MockLedgerEvent { block }),
+            Box::new(|block: Option<MockBlock>| MockLedgerEvent {
+                requester: monad_types::NodeId(get_key(0).pubkey()),
+                bid: monad_types::BlockId(monad_types::Hash([0x00_u8; 32])),
+                block,
+            }),
         )]);
         let retrieved = mock_ledger.next().now_or_never();
         assert_ne!(retrieved, None); // there should be a response
@@ -232,12 +281,126 @@ mod tests {
         mock_ledger.exec(vec![LedgerCommand::LedgerFetch(
             monad_types::NodeId(get_key(0).pubkey()),
             monad_types::BlockId(monad_types::Hash([0x10_u8; 32])),
-            Box::new(|block: Option<MockBlock>| MockLedgerEvent { block }),
+            Box::new(|block: Option<MockBlock>| MockLedgerEvent {
+                requester: monad_types::NodeId(get_key(0).pubkey()),
+                bid: monad_types::BlockId(monad_types::Hash([0x00_u8; 32])),
+                block,
+            }),
         )]);
         let retrieved = mock_ledger.next().now_or_never();
         assert_ne!(retrieved, None); // there should be a response
         let mock_ledger_event = retrieved.unwrap().unwrap().block;
 
         assert_eq!(mock_ledger_event, None);
+    }
+
+    /**
+     *  Fuzz testing on a set of request that is being shuffled into arbitrary order
+     */
+    #[test_case(123123, 12; "test 1")]
+    #[test_case(345345345, 23; "test 2")]
+    #[test_case(346346353, 100; "test 3")]
+    #[test_case(343413452, 100; "test 4")]
+    #[test_case(23452345245, 64; "test 5")]
+    #[test_case(342342, 67; "test 6")]
+    #[test_case(346243632, 97; "test 7")]
+    #[test_case(23534366, 44; "test 8")]
+    #[test_case(457643563, 2; "test 9")]
+    #[test_case(965864809, 1; "test 10")]
+    fn test_ledger_command_arbitrary_order(seed: u64, node_req_range: i32) {
+        assert!(node_req_range > 0);
+        let mut rng = ChaCha20Rng::seed_from_u64(seed);
+        let poll_pref = rng.gen_range(0.0..1.0);
+        let mut mock_ledger = MockLedger::<MockBlock, MockLedgerEvent>::default();
+        assert_eq!(mock_ledger.next().now_or_never(), None); // nothing should be within the pipeline
+
+        let blocks: Vec<_> = (1..40_u8)
+            .map(|seed| MockBlock {
+                block_id: monad_types::BlockId(monad_types::Hash([seed; 32])),
+                parent_block_id: monad_types::BlockId(monad_types::Hash([seed - 1; 32])),
+            })
+            .collect();
+
+        let pub_keys: Vec<_> = (0..100).map(|i| get_key(i).pubkey()).collect();
+        let mut callback_map = HashMap::new();
+        let mut inserted_block = HashSet::new();
+        let mut requests = pub_keys.into_iter().fold(vec![], |mut reqs, key| {
+            for _ in 0..rng.gen_range(1..node_req_range + 1) {
+                let bid = blocks
+                    .choose(&mut rng)
+                    .expect("at least 1 element is within blocks")
+                    .block_id;
+                let node_id = monad_types::NodeId(key);
+                reqs.push(LedgerCommand::LedgerFetch(
+                    node_id,
+                    bid,
+                    Box::new(move |block: Option<MockBlock>| MockLedgerEvent {
+                        requester: node_id,
+                        bid,
+                        block,
+                    }),
+                ));
+            }
+
+            reqs
+        });
+        requests.extend(
+            blocks
+                .clone()
+                .into_iter()
+                .map(|b| LedgerCommand::LedgerCommit(vec![b])),
+        );
+        requests.shuffle(&mut rng);
+
+        while !requests.is_empty() || mock_ledger.ready() {
+            if rng.gen_bool(poll_pref) && !requests.is_empty() {
+                let request = requests.remove(0);
+                match request {
+                    LedgerCommand::LedgerFetch(id, bid, callback) => {
+                        mock_ledger.exec(vec![LedgerCommand::LedgerFetch(id, bid, callback)]);
+                        *callback_map.entry((id, bid)).or_insert(0) = 1;
+                    }
+                    LedgerCommand::LedgerFetchReset(id, bid) => {
+                        mock_ledger.exec(vec![LedgerCommand::LedgerFetchReset(id, bid)]);
+                        if let Entry::Occupied(mut entry) = callback_map.entry((id, bid)) {
+                            *entry.get_mut() = 0;
+                        } else {
+                            panic!("requesting a block that was not planed")
+                        }
+                    }
+                    LedgerCommand::LedgerCommit(blocks) => {
+                        for b in blocks.iter() {
+                            inserted_block.insert(b.block_id);
+                        }
+                        mock_ledger.exec(vec![LedgerCommand::LedgerCommit(blocks)]);
+                    }
+                };
+            } else if let Some(retrieved) = mock_ledger.next().now_or_never() {
+                let result = retrieved.unwrap();
+                let block_id = result.bid;
+                let requester = result.requester;
+
+                if result.block.is_none() {
+                    assert!(!inserted_block.contains(&block_id))
+                }
+                // now also insert the fetch reset
+                if requests.is_empty() {
+                    requests.push(LedgerCommand::LedgerFetchReset(requester, block_id))
+                } else {
+                    requests.insert(
+                        rng.gen_range(0..requests.len()),
+                        LedgerCommand::LedgerFetchReset(requester, block_id),
+                    )
+                }
+            }
+        }
+        for (_, v) in callback_map {
+            // its possible that same block is requested twice by the same user
+            // but only 1 reset is needed since the duplicate request should be ignored
+            assert!(v == 0)
+        }
+        assert!(requests.is_empty());
+        assert!(mock_ledger.blockchain.len() == blocks.len());
+        assert!(!mock_ledger.ready());
     }
 }
