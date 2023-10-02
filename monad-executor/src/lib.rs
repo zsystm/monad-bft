9aa135bfb9971cb86dc17f6519b56c437b04e43e
Aashish Welling <aashishwelling@gmail.com>
2023-10-02 11:34:36 -0400

    Consolidate some obsolete files

--- monad-executor/src/lib.rs
+++ monad-executor/src/lib.rs
@@ -1,6 +1,30 @@
-mod state;
-pub use state::*;
-
-pub mod executor;
 pub mod replay_nodes;
 pub mod timed_event;
+
+use monad_consensus_types::block::BlockType;
+use monad_executor_glue::{Command, Message};
+
+pub trait Executor {
+    type Command;
+    fn exec(&mut self, commands: Vec<Self::Command>);
+}
+
+pub trait State: Sized {
+    type Config;
+    type Event: Clone;
+    type OutboundMessage: Into<Self::Message> + AsRef<Self::Message>;
+    type Message: Message<Event = Self::Event>;
+    type Block: BlockType;
+    type Checkpoint;
+
+    fn init(
+        config: Self::Config,
+    ) -> (
+        Self,
+        Vec<Command<Self::Message, Self::OutboundMessage, Self::Block, Self::Checkpoint>>,
+    );
+    fn update(
+        &mut self,
+        event: Self::Event,
+    ) -> Vec<Command<Self::Message, Self::OutboundMessage, Self::Block, Self::Checkpoint>>;
+}
