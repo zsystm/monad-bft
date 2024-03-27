f39e5951e0dadaa2256d1be2cfaeb674619da5f6
Michael Chen <michaelchen0115@gmail.com>
2024-03-27 11:32:14 -0400

    Devnet integration test
    Adding a flexnet test for devnet images. The script can be run both as
    an integration test and as a utility to spin up a devnet node and rpc
    server for testing purposes.
    
    Co-authored-by: eerkaijun <kaijuneer@gmail.com>
    Co-authored-by: Michael Chen <michaelchen0115@gmail.com>

---
+++ docker/flexnet/nets/devnet-integration/README.md
@@ -0,0 +1,19 @@
+## Devnet integration
+
+A local devnet consists of running a Monad node and a RPC server (which is the interface to send requests to the node).
+
+### Script usage
+```
+scripts/net-run.sh <mode> --output-dir <output_path> --net-dir <net_path> --image-root <image_root> --monad-bft-root <monad_bft_root>
+```
+where `mode` can be `run` or `test`.
+
+The script creates a unique directory in `output_path` and spins up the services from there
+
+**Mode 1: Run** 
+
+Spin up `monad-node` and `monad_rpc` services. `monad_rpc` server can be accessed at `http://localhost:8080`. The service root path is printed at the end of the script. 
+
+**Mode 2: Test**
+
+Runs an integration test to sanity check the devnet setup.
---
+++ docker/flexnet/nets/devnet-integration/scripts/net-run.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+set -ex
+# Define the function to show usage
+usage() {
+    echo "Usage: $0 run/test --output-dir <dir_path> --net-dir <net_path> --image-root <image_root> --monad-bft-root <monad_bft_root>"
+    exit 1
+}
+
+
+# Initialize variables
+output_dir=""
+net_dir=""
+image_root=""
+monad_bft_root=""
+mode=""
+
+
+if [ "$#" -eq 0 ]; then
+    usage; exit 1
+fi
+
+# Extract options and their arguments into variables
+while true; do
+    case "$1" in
+        --output-dir)
+            case "$2" in
+                "") echo "$1 must have a value"; shift 1 ;;
+                *)  output_dir="$2"; shift 2 ;;
+            esac ;;
+        --net-dir)
+            case "$2" in
+                "") echo "$1 must have a value"; shift 1 ;;
+                *)  net_dir="$2"; shift 2 ;;
+            esac ;;
+        --image-root)
+            case "$2" in 
+                "") echo "$1 must have a value"; shift 1 ;;
+                *)  image_root="$2"; shift 2 ;;
+            esac ;;
+        --monad-bft-root)
+            case "$2" in
+                "") echo "$1 must have a value"; shift 1 ;;
+                *)  monad_bft_root="$2"; shift 2;;
+            esac ;;
+        run) shift 1; mode="run" ;;
+        test) shift 1; mode="test" ;;
+        "") break ;;
+        *) echo "Error parsing $1, check arguments before it"; usage; exit 1 ;;
+    esac
+done
+
+# Verify the arguments 
+if [ -z "$output_dir" ]; then
+    echo "Error: --output-dir is required."
+    usage
+elif [ ! -d "$output_dir" ]; then
+    echo "Error: --output-dir doesn't exist"
+    exit 1
+fi
+
+if [ -z "$net_dir" ]; then
+    echo "Error: --net-dir is required."
+    usage
+elif [ ! -d "$net_dir" ]; then
+    echo "Error: --net-dir is not a directory."
+    exit 1
+fi
+
+if [ -z "$image_root" ]; then
+    echo "Error: --image-root is required."
+    usage
+elif [ ! -d "$image_root" ]; then
+    echo "Error: --image-root is not a directory."
+    exit 1
+fi
+
+if [ -z "$monad_bft_root" ]; then
+    echo "Error: --monad-bft-root is required."
+    usage
+elif [ ! -d "$monad_bft_root" ]; then
+    echo "Error: --monad-bft-root is not a directory."
+    exit 1
+fi
+
+devnet_dir="$monad_bft_root/docker/devnet"
+rpc_dir="$monad_bft_root/docker/rpc"
+
+if [ ! -d "$devnet_dir" ]; then
+    echo "devnet dir $devnet_dir is not a directory"
+    exit 1
+fi
+
+if [ ! -d "$rpc_dir" ]; then
+    echo "rpc dir $rpc_dir is not a directory"
+    exit 1
+fi
+
+# Create node volume directory
+net_name=$(basename $(realpath "$net_dir"))
+rand_hex=$(od -vAn -N8 -tx1 /dev/urandom | tr -d " \n" | cut -c 1-16)
+vol_root="$output_dir/$net_name-$(date +%Y%m%d_%H%M%S)-$rand_hex"
+
+mkdir $vol_root
+echo "Root of node volumes created at: $vol_root"
+
+# set up output dir
+mkdir -p $vol_root/node
+cp -r $net_dir/* $vol_root
+cp -r $devnet_dir/monad/config $vol_root/node
+
+
+# Set environment variables
+export FLEXNET_IMAGE_ROOT=$(realpath "$image_root")
+export MONAD_BFT_ROOT=$(realpath "$monad_bft_root")
+export HOST_GID=$(id -g)
+export HOST_UID=$(id -u)
+export DEVNET_DIR=$(realpath "$devnet_dir")
+export RPC_DIR=$(realpath "$rpc_dir")
+
+
+if [ "$mode" == "run" ]; then
+    cd $vol_root
+    docker compose up monad_node monad_rpc
+
+    exit 0
+elif [ "$mode" == "test" ]; then
+    pushd $vol_root
+
+    build_services=$(docker compose config --services | grep build)
+    runner_services=$(docker compose config --services | grep runner)
+    node_services=$(docker compose config --services | grep -v -E "(build|runner)")
+
+    docker compose build $build_services &&
+    docker compose build $runner_services &&
+    docker compose up --detach $node_services
+    sleep 10
+    docker compose down $node_services
+
+    popd # $vol_root
+
+    # inspect the blocks, verify content
+    docker run --rm -v ./$vol_root:/monad monad-python bash -c "python3 /monad/scripts/inspect-block.py --data /monad/data/txns.json"
+    exit 0
+else
+    echo "Unsupported mode $mode"
+fi
