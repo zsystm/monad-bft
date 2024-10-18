cce7254288d92b335c649ed42b61239f2cab4fac
Joe Howarth <joe@monad.xyz>
2024-10-18 14:45:00 -0400

    simple erc20 test binary

---
+++ monad-eth-testutil/examples/erc20-sender/main.rs
@@ -0,0 +1,251 @@
+use std::{
+    fs::File,
+    path::{Path, PathBuf},
+    process::exit,
+    time::Duration,
+};
+
+use account::PrivateKey;
+use alloy_json_rpc::RpcReturn;
+use alloy_rpc_client::{ClientBuilder, ReqwestClient};
+use alloy_sol_macro::sol;
+use alloy_sol_types::SolCall;
+use clap::{command, Parser};
+use eyre::{Context, ContextCompat, Result};
+use reth_primitives::{
+    hex::FromHex, AccessList, Address, Bytes, Transaction, TransactionKind, TransactionSigned,
+    TxEip1559, U256, U64,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::{json, Value};
+use url::Url;
+
+mod account;
+
+#[derive(Debug, Parser)]
+#[command( about, long_about = None)]
+pub struct EthTxGenCli {
+    // #[arg(long, default_value = "http://localhost:8080")]
+    #[arg(long, default_value = "http://127.0.0.1:8545")] // anvil
+    pub rpc_url: Url,
+
+    #[arg(long, default_value = "./ERC20.json")]
+    pub path_to_contract: PathBuf,
+
+    #[arg(
+        long,
+        // anvil
+        default_value = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"
+    )]
+    pub private_key: String,
+}
+
+#[derive(Deserialize, Debug)]
+#[repr(transparent)]
+pub struct NonceResult(pub String);
+
+impl NonceResult {
+    fn nonce(self) -> Result<u64> {
+        u64::from_str_radix(
+            self.0
+                .strip_prefix("0x")
+                .context("nonce string always has 0x prefix")?,
+            16,
+        )
+        .context("nonce string is valid u64")
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = EthTxGenCli::parse();
+    let client: ReqwestClient = ClientBuilder::default().http(args.rpc_url);
+
+    let output_file = File::open(args.path_to_contract)?;
+    let json = serde_json::from_reader(output_file)?;
+    let bytecode: String =
+        extract_bytecode_string(json).context("Couldn't parse bytecode from file")?;
+
+    let (addr, private_key) = PrivateKey::new(args.private_key);
+    let nonce = client
+        .request::<_, NonceResult>(
+            "eth_getTransactionCount",
+            [addr.to_string(), "latest".to_string()],
+        )
+        .await?
+        .nonce()?;
+
+    println!("Nonce: {nonce}");
+    // println!("Bytecode: {}", bytecode);
+
+    let mut input = Bytes::from_hex(bytecode)?.to_vec();
+
+    let tx = Transaction::Eip1559(TxEip1559 {
+        chain_id: 41454,
+        nonce,
+        gas_limit: 4_000_000,
+        max_fee_per_gas: 100_000,
+        max_priority_fee_per_gas: 10,
+        to: TransactionKind::Create,
+        value: U256::ZERO.into(),
+        access_list: AccessList::default(),
+        input: input.into(),
+    });
+
+    let gas_estimate: u64 = client
+        .request::<_, U64>("eth_estimateGas", [&tx])
+        .await?
+        .to();
+
+    println!("gas estimate: {}", gas_estimate);
+
+    let sig = private_key.sign_transaction(&tx)?;
+    let tx = TransactionSigned::from_transaction_and_signature(tx, sig);
+
+    let tx_hash = client
+        .request::<_, String>("eth_sendRawTransaction", [tx.envelope_encoded()])
+        .await?;
+
+    println!("Tx Hash: {tx_hash}");
+
+    let rx = loop {
+        let rx = client
+            .request::<_, Value>("eth_getTransactionReceipt", [&tx_hash])
+            .await?;
+        if !rx.is_null() {
+            break rx;
+        }
+        tokio::time::sleep(Duration::from_millis(200)).await;
+    };
+    println!(
+        "Construct: status {}, rx: {}",
+        &rx["status"],
+        serde_json::to_string(&rx)?
+    );
+    let contract_address = rx
+        .get("contractAddress")
+        .context("Failed to parse contract address from receipt")?
+        .as_str()
+        .unwrap();
+    let contract_address = Address::from_hex(contract_address)?;
+
+    // try transfering
+    let nonce = client
+        .request::<_, NonceResult>(
+            "eth_getTransactionCount",
+            [addr.to_string(), "latest".to_string()],
+        )
+        .await?
+        .nonce()?;
+
+    let input = ERC20::mintCall {}.abi_encode();
+
+    let tx = Transaction::Eip1559(TxEip1559 {
+        chain_id: 41454,
+        nonce,
+        gas_limit: 4_000_000,
+        max_fee_per_gas: 100_000,
+        max_priority_fee_per_gas: 10,
+        to: TransactionKind::Call(contract_address),
+        value: U256::ZERO.into(),
+        access_list: AccessList::default(),
+        input: input.into(),
+    });
+
+    let gas_estimate: u64 = client
+        .request::<_, U64>("eth_estimateGas", [&tx])
+        .await?
+        .to();
+
+    println!("gas estimate: {}", gas_estimate);
+
+    let sig = private_key.sign_transaction(&tx)?;
+    let tx = TransactionSigned::from_transaction_and_signature(tx, sig);
+
+    let tx_hash = client
+        .request::<_, String>("eth_sendRawTransaction", [tx.envelope_encoded()])
+        .await?;
+
+    let rx = loop {
+        let rx = client
+            .request::<_, Value>("eth_getTransactionReceipt", [&tx_hash])
+            .await?;
+        if !rx.is_null() {
+            break rx;
+        }
+        tokio::time::sleep(Duration::from_millis(200)).await;
+    };
+    println!(
+        "Mint: status: {}, rx: {}",
+        rx["status"],
+        serde_json::to_string(&rx)?
+    );
+
+    // total supply
+
+    {
+        let call = json!({
+            "to": contract_address,
+            "data": ERC20::totalSupplyCall {}.abi_encode()
+        });
+
+        let supply: U256 = client.request("eth_call", [call]).await?;
+        println!("Supply {supply}");
+    }
+
+    {
+        // balanceOf
+        let contract = Contract {
+            addr: contract_address,
+            client: client.clone(),
+        };
+        let balance: U256 = contract
+            .call(ERC20::balanceOfCall {
+                account: addr.clone(),
+            })
+            .await?;
+
+        println!("Balance {balance}");
+    }
+
+    Ok(())
+}
+
+struct Contract {
+    pub addr: Address,
+    pub client: ReqwestClient,
+}
+
+impl Contract {
+    pub async fn call<R: RpcReturn, T: alloy_sol_types::SolCall>(&self, input: T) -> Result<R> {
+        let call = json!({
+            "to": self.addr,
+            "data": input.abi_encode()
+        });
+
+        let x = self.client.request::<_, R>("eth_call", [call]).await?;
+        Ok(x)
+    }
+}
+
+fn extract_bytecode_string(obj: Value) -> Option<String> {
+    obj.get("bytecode")?
+        .get("object")?
+        .as_str()
+        .map(ToOwned::to_owned)
+}
+
+sol! {
+pragma solidity ^0.8.13;
+
+contract ERC20 {
+    // constructor(string memory _name, string memory _symbol, uint8 _decimals);
+
+    function mint() external;
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+}}
