fd1bae99fdb337ad66757897b69200666d1ab7d7
Aashish Welling <aashishwelling@gmail.com>
2024-11-19 15:02:48 -0500

    Remove triedb_driver_mock

--- monad-triedb/build.rs
+++ monad-triedb/build.rs
@@ -4,15 +4,20 @@ fn main() {
     println!("cargo:rerun-if-changed=triedb-driver");
     println!("cargo:rerun-if-changed=../monad-cxx/monad-execution");
     println!("cargo:rerun-if-env-changed=TRIEDB_TARGET");
-    let target = env::var("TRIEDB_TARGET").unwrap_or("triedb_driver_mock".to_owned());
-    let dst = cmake::Config::new("triedb-driver")
-        .define("CMAKE_BUILD_TARGET", &target)
-        .build_target(&target)
-        .build();
+    let build_execution_lib =
+        env::var("TRIEDB_TARGET").is_ok_and(|target| target == "triedb_driver");
+    if build_execution_lib {
+        let target = "triedb_driver";
 
-    println!("cargo:rustc-link-arg=-Wl,-rpath,{}", dst.display());
-    println!("cargo:rustc-link-search=native={}/build", dst.display());
-    println!("cargo:rustc-link-lib=dylib={}", &target);
+        let dst = cmake::Config::new("triedb-driver")
+            .define("CMAKE_BUILD_TARGET", target)
+            .build_target(target)
+            .build();
+
+        println!("cargo:rustc-link-arg=-Wl,-rpath,{}", dst.display());
+        println!("cargo:rustc-link-search=native={}/build", dst.display());
+        println!("cargo:rustc-link-lib=dylib={}", target);
+    }
 
     let bindings = bindgen::Builder::default()
         .header("triedb-driver/include/triedb.h")
