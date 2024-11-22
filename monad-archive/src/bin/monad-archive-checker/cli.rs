ed03fc3eaef33af7194a5af6e9fd1248384f9474
Tong Zhi <tong@monad.xyz>
2024-11-22 11:24:09 -0500

    Add S3ArchiverReader interface and S3 buckets checker program
    Add S3 buckets checker program
    
    format
    
    Resolve merge conflicts
    
    Refactor

---
+++ monad-archive/src/bin/monad-archive-checker/cli.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[command(name = "monad-archive-checker", about, long_about = None)]
+pub struct Cli {
+    #[arg(long, value_delimiter = ',')]
+    pub s3_buckets: Vec<String>,
+
+    #[arg(long, value_delimiter = ',')]
+    pub regions: Vec<String>,
+
+    /// Set start checking block number
+    #[arg(long, default_value_t = 0)]
+    pub start_block_number: u64,
+
+    /// TODO: Set the concurrent blocks
+    #[arg(long, default_value_t = 10)]
+    pub max_concurrent_blocks: usize,
+
+    /// Set max block processed per iter
+    #[arg(long, default_value_t = 200)]
+    pub max_blocks_per_iteration: u64,
+
+    /// Set max tolerated log for each bucket, should be much SMALLER than "max_blocks_per_iteration"
+    #[arg(long, default_value_t = 25)]
+    pub max_lag: u64,
+}
