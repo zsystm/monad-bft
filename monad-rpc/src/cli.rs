fd838bc9ac35e1be6f6554dfd601086d2a6017a1
Aashish Welling <aashishwelling@gmail.com>
2025-01-08 18:00:11 -0500

    Hard-coded devnet chain-id

--- monad-rpc/src/cli.rs
+++ monad-rpc/src/cli.rs
@@ -26,7 +26,7 @@ pub struct Cli {
     pub rpc_port: u16,
 
     /// Set the chain ID
-    #[arg(long, default_value_t = 10143)]
+    #[arg(long, default_value_t = 20143)]
     pub chain_id: u64,
 
     /// Set the max number of requests in a batch request
