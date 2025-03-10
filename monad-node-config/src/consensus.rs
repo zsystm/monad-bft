450cc3b35b9755ee60af34c847b5dc0668a192fd
Bharath Amrithraj <bamrithraj@category.xyz>
2025-03-10 11:42:20 -0400

    Remove block_txn_limit

--- monad-node-config/src/consensus.rs
+++ monad-node-config/src/consensus.rs
@@ -3,6 +3,5 @@ use serde::Deserialize;
 #[derive(Debug, Clone, Deserialize)]
 #[serde(deny_unknown_fields)]
 pub struct NodeConsensusConfig {
-    pub block_txn_limit: usize,
     pub execution_delay: u64,
 }
