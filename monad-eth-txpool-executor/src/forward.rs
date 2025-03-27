0117f2566a232049296815118386f7f0606133da
Andre Benedito <38482987+andr-dev@users.noreply.github.com>
2025-03-27 12:08:31 -0400

    Add txpool executor forwarding manager tests

--- monad-eth-txpool-executor/src/forward.rs
+++ monad-eth-txpool-executor/src/forward.rs
@@ -150,3 +150,269 @@ impl EthTxPoolForwardingManagerProjected<'_> {
         }
     }
 }
+
+#[cfg(test)]
+mod test {
+    use std::{
+        pin::{pin, Pin},
+        task::{Context, Poll},
+        time::Duration,
+    };
+
+    use alloy_consensus::{transaction::Recovered, Transaction, TxEnvelope};
+    use alloy_primitives::{hex, B256};
+    use futures::task::noop_waker_ref;
+    use itertools::Itertools;
+    use monad_eth_testutil::{make_legacy_tx, recover_tx};
+    use monad_eth_types::BASE_FEE_PER_GAS;
+
+    use crate::forward::{
+        EthTxPoolForwardingManager, INGRESS_CHUNK_INTERVAL_MS, INGRESS_CHUNK_MAX_SIZE,
+    };
+
+    // pubkey starts with AAA
+    const S1: B256 = B256::new(hex!(
+        "0ed2e19e3aca1a321349f295837988e9c6f95d4a6fc54cfab6befd5ee82662ad"
+    ));
+
+    fn setup<'a>() -> (EthTxPoolForwardingManager, Context<'a>) {
+        (
+            EthTxPoolForwardingManager::new(),
+            Context::from_waker(noop_waker_ref()),
+        )
+    }
+
+    fn generate_tx(nonce: u64) -> Recovered<TxEnvelope> {
+        recover_tx(make_legacy_tx(
+            S1,
+            BASE_FEE_PER_GAS as u128,
+            100_000,
+            nonce,
+            0,
+        ))
+    }
+
+    async fn assert_pending_now_and_forever(
+        mut forwarding_manager: Pin<&mut EthTxPoolForwardingManager>,
+        mut cx: Context<'_>,
+    ) {
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+        assert_eq!(
+            forwarding_manager.as_mut().poll_egress(&mut cx),
+            Poll::Pending
+        );
+
+        tokio::time::advance(Duration::from_secs(24 * 60 * 60)).await;
+
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+        assert_eq!(
+            forwarding_manager.as_mut().poll_egress(&mut cx),
+            Poll::Pending
+        );
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn test_poll_none() {
+        let (forwarding_manager, cx) = setup();
+        let forwarding_manager = pin!(forwarding_manager);
+
+        assert_pending_now_and_forever(forwarding_manager, cx).await;
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn test_ingress_simple() {
+        for poll_ingress_before_insert in [false, true] {
+            let (forwarding_manager, mut cx) = setup();
+            let mut forwarding_manager = pin!(forwarding_manager);
+
+            if poll_ingress_before_insert {
+                assert_eq!(
+                    forwarding_manager.as_mut().poll_ingress(&mut cx),
+                    Poll::Pending
+                );
+            }
+
+            let txs = vec![generate_tx(0)];
+
+            forwarding_manager
+                .as_mut()
+                .project()
+                .add_ingress_txs(txs.clone());
+
+            assert_eq!(
+                forwarding_manager.as_mut().poll_ingress(&mut cx),
+                Poll::Ready(txs.clone())
+            );
+
+            assert_pending_now_and_forever(forwarding_manager, cx).await;
+        }
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn test_ingress_subsequent() {
+        let (forwarding_manager, mut cx) = setup();
+        let mut forwarding_manager = pin!(forwarding_manager);
+
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+
+        let txs = vec![generate_tx(0)];
+
+        forwarding_manager
+            .as_mut()
+            .project()
+            .add_ingress_txs(txs.clone());
+
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Ready(txs.clone())
+        );
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+
+        forwarding_manager
+            .as_mut()
+            .project()
+            .add_ingress_txs(txs.clone());
+
+        // Since time is frozen and we just polled, the forwarding manager should wait its interval
+        // even though it should be "empty"
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+
+        tokio::time::advance(
+            Duration::from_millis(INGRESS_CHUNK_INTERVAL_MS)
+                .checked_sub(Duration::from_nanos(1))
+                .unwrap(),
+        )
+        .await;
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+
+        tokio::time::advance(Duration::from_nanos(1)).await;
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Ready(txs.clone())
+        );
+
+        assert_pending_now_and_forever(forwarding_manager, cx).await;
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn test_ingress_chunks() {
+        let (forwarding_manager, mut cx) = setup();
+        let mut forwarding_manager = pin!(forwarding_manager);
+
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+
+        const NUM_CHUNKS: usize = 16;
+
+        // We insert the last tx below to test adding to an existing chunk
+        const NUM_TXS: usize = INGRESS_CHUNK_MAX_SIZE * NUM_CHUNKS - 1;
+
+        forwarding_manager
+            .as_mut()
+            .project()
+            .add_ingress_txs((0..NUM_TXS as u64).map(generate_tx).collect_vec());
+
+        for chunk_num in 0..NUM_CHUNKS {
+            tokio::time::advance(Duration::from_millis(INGRESS_CHUNK_INTERVAL_MS)).await;
+
+            if chunk_num + 1 == NUM_CHUNKS {
+                forwarding_manager
+                    .as_mut()
+                    .project()
+                    .add_ingress_txs(vec![generate_tx(0)]);
+            }
+
+            let Poll::Ready(txs) = forwarding_manager.as_mut().poll_ingress(&mut cx) else {
+                panic!("forwarding manager should be ready after each iteration");
+            };
+
+            assert_eq!(txs.len(), INGRESS_CHUNK_MAX_SIZE);
+
+            // Check that txs are produced in the same order they are inserted
+            txs.into_iter().enumerate().for_each(|(idx, tx)| {
+                // By using % NUM_TXS, we can check that the last tx is the 0 nonce added above when
+                // we're at the last chunk
+                assert_eq!(
+                    tx.nonce(),
+                    ((idx + chunk_num * INGRESS_CHUNK_MAX_SIZE) as u64) % (NUM_TXS as u64)
+                );
+            });
+
+            assert_eq!(
+                forwarding_manager.as_mut().poll_ingress(&mut cx),
+                Poll::Pending
+            );
+        }
+
+        assert_pending_now_and_forever(forwarding_manager, cx).await;
+    }
+
+    #[tokio::test(start_paused = true)]
+    async fn test_ingress_complete() {
+        let (forwarding_manager, mut cx) = setup();
+        let mut forwarding_manager = pin!(forwarding_manager);
+
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+
+        forwarding_manager.as_mut().project().add_ingress_txs(
+            (0..2 * INGRESS_CHUNK_MAX_SIZE as u64)
+                .map(generate_tx)
+                .collect_vec(),
+        );
+
+        let Poll::Ready(txs) = forwarding_manager.as_mut().poll_ingress(&mut cx) else {
+            panic!("forwarding manager should be ready");
+        };
+        assert_eq!(txs.len(), INGRESS_CHUNK_MAX_SIZE);
+
+        tokio::time::advance(Duration::from_millis(1)).await;
+
+        forwarding_manager.as_mut().complete_ingress();
+
+        tokio::time::advance(
+            Duration::from_millis(INGRESS_CHUNK_INTERVAL_MS)
+                .checked_sub(Duration::from_millis(1))
+                .unwrap(),
+        )
+        .await;
+
+        // Even though we have advanced INGRESS_CHUNK_INTERVAL_MS, the forwarding manager should
+        // wait an additional 1ms since complete_ingress was called 1ms after the poll.
+        assert_eq!(
+            forwarding_manager.as_mut().poll_ingress(&mut cx),
+            Poll::Pending
+        );
+
+        tokio::time::advance(Duration::from_millis(1)).await;
+
+        let Poll::Ready(txs) = forwarding_manager.as_mut().poll_ingress(&mut cx) else {
+            panic!("forwarding manager should be ready");
+        };
+        assert_eq!(txs.len(), INGRESS_CHUNK_MAX_SIZE);
+
+        assert_pending_now_and_forever(forwarding_manager, cx).await;
+    }
+}
