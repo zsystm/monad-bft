e0176bb2f73ecc0279bdef7bae51993955cd8d4f
Aashish Welling <aashishwelling@gmail.com>
2025-07-26 10:09:42 -0400

    Update clear-old-artifacts to new ledger format

--- debian/opt/monad/scripts/clear-old-artifacts.sh
+++ debian/opt/monad/scripts/clear-old-artifacts.sh
@@ -5,10 +5,10 @@ TARGET_DIR="/home/monad/monad-bft/ledger/"
 NEW_FILES=$(find "$TARGET_DIR" -type f -name "*" -mmin -20)
 if [ -n "$NEW_FILES" ]; then
     find /home/monad/monad-bft/config/forkpoint/ -type f -name "forkpoint.toml.*" -mmin +300 -delete 2>/dev/null
-    find /home/monad/monad-bft/ledger/ -type f -name "*.body" -mmin +600 -delete 2>/dev/null
-    find /home/monad/monad-bft/ledger/ -type f -name "*.header" -mmin +600 -delete 2>/dev/null
+    find /home/monad/monad-bft/ledger/headers -type f -mmin +600 -delete 2>/dev/null
+    find /home/monad/monad-bft/ledger/bodies -type f -mmin +600 -delete 2>/dev/null
     find /home/monad/monad-bft/ -type f -name "wal_*" -mmin +300 -delete 2>/dev/null
 else
-    echo "No new files detected. Skipping deletion of .header files."
+    echo "No new files detected. Skipping deletion of ledger files."
 fi
 exit 0
